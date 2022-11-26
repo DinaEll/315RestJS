be8046eb7c61baa5327eb05d55d8aa4cf522cfdc
Dina Pupina <goncharova.hope@gmail.com>
2022-11-26 21:05:51 +0300

    24/11

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRESTController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminRESTController.java
@@ -7,46 +7,54 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
 import ru.kata.spring.boot_security.demo.service.RoleServiceImpl;
+import ru.kata.spring.boot_security.demo.service.UserService;
 import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class AdminRESTController {
 
-    private UserServiceImpl userService;
-    private RoleServiceImpl roleService;
+    private UserService userService;
+    private RoleService roleService;
 
     @Autowired
-    public AdminRESTController(UserServiceImpl userService, RoleServiceImpl roleService) {
+    public AdminRESTController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
     @GetMapping("/users")
     public ResponseEntity<List<User>> showAllUsers() {
         List<User> users = userService.getListUsers();
-        return users != null && !users.isEmpty()
-                ? new ResponseEntity<>(users, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        try {
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/users/{id}")
     public ResponseEntity<User> showUser(@PathVariable int id) {
         User user = userService.getById(id);
-        return user != null
-                ? new ResponseEntity<>(user, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        try {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> addNewUser(@RequestBody User user) {
+    public ResponseEntity<User> addNewUser(@RequestBody @Valid User user) {
         userService.add(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
     @PutMapping("/users/{id}")
-    public ResponseEntity<User> updateUser(@RequestBody User user) {
+    public ResponseEntity<User> updateUser(@RequestBody @Valid User user) {
         userService.update(user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
