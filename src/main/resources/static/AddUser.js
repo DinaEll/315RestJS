eaeb74306e1cf2be3fe8856eaa2ac35061f277bd
Dina Pupina <goncharova.hope@gmail.com>
2022-11-26 18:46:31 +0300

    26/11

--- src/main/resources/static/AddUser.js
+++ src/main/resources/static/AddUser.js
@@ -1,3 +1,6 @@
+
+//Добавление нового юзера
+
 $(async function() {await newUser();});
 
 async function newUser() {
@@ -42,4 +45,105 @@ async function newUser() {
             $('#allUsersTable').click();
         })
     }
+} //Добавление нового юзера
+
+
+//Отображение всех юзеров на страничке админа
+
+$(async function () {await allUsers();});
+
+const table = $('#tbodyAllUserTable');
+
+async function allUsers() {
+    table.empty()
+    fetch("http://localhost:8080/api/users")
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(user => {
+                let tableWithUsers = `$(
+                        <tr><td>${user.id}</td>
+                            <td>${user.firstName}</td>
+                            <td>${user.age}</td>
+                            <td>${user.email}</td>
+                            <td>${user.roles.map(role => " " + role.role.substring(5))}</td>
+                            <td><button type="button" class="btn btn-info" data-toggle="modal" id="buttonEdit"
+                                data-action="edit" data-id="${user.id}" data-target="#edit">Edit</button>
+                            </td>
+                            
+                            <td><button type="button" class="btn btn-danger" data-toggle="modal" id="buttonDelete"
+                                data-action="delete" data-id="${user.id}" data-target="#delete">Delete</button>
+                            </td>
+                        </tr>)`;
+                table.append(tableWithUsers);
+            })
+        })
+} //Отображение всех юзеров на страничке админа
+
+//Удаление модели юзера
+
+$('#delete').on('show.bs.modal', ev => {
+    let button = $(ev.relatedTarget);
+    let id = button.data('id');
+    showDeleteModal(id);
+})
+
+async function showDeleteModal(id) {
+    let user = await getUser(id);
+    let form = document.forms["formDeleteUser"];
+    form.id.value = user.id;
+    form.firstName.value = user.firstName;
+    form.password.value = user.password;
+    form.age.value = user.age;
+    form.email.value = user.email;
+
+
+    $('#rolesDeleteUser').empty();
+
+    await fetch("http://localhost:8080/api/roles")
+        .then(res => res.json())
+        .then(roles => {
+            roles.forEach(role => {
+                let selectedRole = false;
+                for (let i = 0; i < user.roles.length; i++) {
+                    if (user.roles[i].name === role.role) {
+                        selectedRole = true;
+                        break;
+                    }
+                }
+                let el = document.createElement("option");
+                el.text = role.role.substring(5);
+                el.value = role.id;
+                if (selectedRole) el.selected = true;
+                $('#rolesDeleteUser')[0].appendChild(el);
+            })
+        });
 }
+
+async function getUser(id) {
+    let url = "http://localhost:8080/api/users/" + id;
+    let response = await fetch(url);
+    return await response.json();
+} //Удаление модели юзера
+
+//Удаление самого юзера
+
+$(async function() {deleteUser();});
+
+function deleteUser(){
+    const deleteForm = document.forms["formDeleteUser"];
+    deleteForm.addEventListener("submit", ev => {
+        ev.preventDefault();
+        fetch("http://localhost:8080/api/users/" + deleteForm.id.value, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(() => {$('#deleteFormCloseButton').click();
+                allUsers();
+            })
+    })
+} //Удаление самого юзера
+
+
+
