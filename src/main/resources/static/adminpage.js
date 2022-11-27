e51aceb206c92ba4e18283f66aac6dc1115c8474
Dina Pupina <goncharova.hope@gmail.com>
2022-11-27 19:25:45 +0300

    26/11

--- src/main/resources/static/adminpage.js
+++ src/main/resources/static/adminpage.js
@@ -5,12 +5,10 @@ async function thisUser() {
     fetch("http://localhost:8080/api/viewUser")
         .then(res => res.json())
         .then(data => {
-            // Инфа в шапке
             $('#headerUsername').append(data.username);
             let roles = data.roles.map(role => " " + role.role.substring(5));
             $('#headerRoles').append(roles);
 
-            //Инфа в таблице
             let user = `$(
             <tr><td>${data.id}</td>
                 <td>${data.firstName}</td>
@@ -227,9 +225,5 @@ function deleteUser(){
             })
     })
 } //Delete user
-
-
-
-
 
 
